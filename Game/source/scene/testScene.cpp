a9551ca0c5aab80b78e1b4ec4d921aaaf65fdfa0
Ferdian <ferdian.artwork@gmail.com>
2021-01-13 11:11:30 +0000

    using latest simplenfl

--- Game/source/scene/testScene.cpp
+++ Game/source/scene/testScene.cpp
@@ -8,8 +8,8 @@ EE::Scene1::Scene1() : Scene() {
 }
 
 void EE::Scene1::update() {
-    if (SNF::getKeys(SNF::Key::L, SNF::KeyPhase::release)) {
-        App::get_instance()->set_mainScene(new Scene2);
+    if (SNF::getTouchRect(30, 30, 50, 50, SNF::KeyPhase::press)) {
+        std::cout << "Touch" << std::endl;
     }
 }
 
@@ -20,7 +20,5 @@ EE::Scene2::Scene2() : Scene() {
 }
 
 void EE::Scene2::update() {
-    if (SNF::getKeys(SNF::Key::L, SNF::KeyPhase::release)) {
-        App::get_instance()->set_mainScene(new Scene1);
-    }
+    
 }
